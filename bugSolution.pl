40413556c6b4571301632e2aad819d249e632cbd
BugHunter-X <askzenai@gmail.com>
2024-12-31 05:20:03 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+use strict; use warnings; use Algorithm::Permute; my %hash = (a => 1, b => 2, c => 3); my @sortedKeys = sort keys %hash; foreach my $key (@sortedKeys) { print "$key => $hash{$key}\n"; }
